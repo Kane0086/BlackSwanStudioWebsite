88b05bfcc2738f9291d305ac8fb4b8912810c5a7
Kane0086 <bassagal.louis@gmail.com>
2023-02-16 20:49:47 +0100

    [ADD] Add carousel instead of Banner

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,20 @@
 import './../styles/App.css';
+import Carousel, { CarouselItem } from './Carousel';
 import Header from './Header';
-import Banner from './Banner';
 import News from './News';
+import BusterSword from '../assets/img/ff7r.jpg';
+import Clive from '../assets/img/ff16.jpg';
+import Nier from '../assets/img/automata.jpg'
 
 function App() {
   return (
     <div>
       <Header />
-      <Banner />
+      <Carousel>
+        <CarouselItem img={BusterSword}> </CarouselItem>
+        <CarouselItem img={Clive}>Item 2</CarouselItem>
+        <CarouselItem img={Nier}>Item 3</CarouselItem>
+      </Carousel>
       <News />
     </div>
   );
---
+++ src/components/Carousel.js
@@ -0,0 +1,65 @@
+import React, { useRef, useState } from 'react';
+
+import '../styles/Carousel.css';
+import { useEffect } from 'react';
+
+export const CarouselItem = ({ children, width, img }) => {
+  return (
+    <div className="carousel-item" style={{ width: width }}>
+      <img className='image' src={img} alt='BlackSwanStudioGames'></img>
+        <div className="image-filter"></div>
+    </div>
+  );
+};
+
+const Carousel = ({ children }) => {
+  const [activeIndex, setActiveIndex] = useState(0);
+  var index = useRef(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      updateIndex(activeIndex + 1);
+    }, 5000);
+    return () => clearInterval(interval);
+  });
+
+  const updateIndex = (newIndex) => {
+    if (newIndex < 0)
+        newIndex = React.Children.count(children) - 1;
+    else if (newIndex >= React.Children.count(children))
+        newIndex = 0;
+    index.current = newIndex;
+    setActiveIndex(newIndex);
+  };
+
+  return (
+    <div className="carousel">
+      <div className="inner" style={{ transform: `translateX(-${activeIndex * 100}%)` }}>
+        {React.Children.map(children, (child, index) => {
+          return React.cloneElement(child, { width: '100%' });
+        })}
+      </div>
+      <div className="dot-container">
+          <li>
+            <ul>
+              <div className="unchecked-dot" onClick={() => {updateIndex(0)}}>
+                <div className={index.current === 0 ? "checked" : ""}></div>
+              </div>
+            </ul>
+            <ul>
+              <div className="unchecked-dot" onClick={() => {updateIndex(1)}}>
+              <div className={index.current === 1 ? "checked" : ""}></div>
+              </div>
+            </ul>
+            <ul>
+              <div className="unchecked-dot" onClick={() => {updateIndex(2)}}>
+              <div className={index.current === 2 ? "checked" : ""}></div>
+              </div>
+            </ul>
+          </li>
+        </div>
+    </div>
+  );
+};
+
+export default Carousel;
