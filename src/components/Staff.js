ecd45e4f10b220fe7b92d99833074522a4ae618f
Kane0086 <bassagal.louis@gmail.com>
2023-07-14 22:34:02 +0200

    [FIX] Fix Name of team member

--- src/components/Staff.js
+++ src/components/Staff.js
@@ -15,13 +15,13 @@ function Staff() {
             <h1 className="staff-title">TEAM</h1>
             <div className="staff-container">
                 <div className="staff-row">
-                    <TeamBox img={andrea} name="POUPARD Andréa" text="Responsable de la direction stratégique de l'entreprise et de la prise de décisions importantes."></TeamBox>
+                    <TeamBox img={andrea} name="POUPARD Andréa" text="Définit la stratégie et la vision globale du studio et veille à la réalisation des opérations quotidiennes."></TeamBox>
                     <TeamBox img={paul} name="LYNCH Paul" text="Définit la stratégie marketing globale et crée du contenu engageant."></TeamBox>
                     <TeamBox img={theo} name="RIVIERE Théo" text="Elabore les visuels des jeux et l'identité graphique du studio."></TeamBox>
                     <TeamBox img={quentin} name="CHARPENTIER Quentin" text="Travaille activement pour rendre l'expérience de jeu agréable."></TeamBox>
                     <TeamBox img={abdoul} name="GUERROUDJ Abdoul" text="Explore différentes perspectives pour enrichir et faire évoluer le gameplay."></TeamBox>
                     <TeamBox img={madeleine} name="PEILLON Madeleine" text="Musicienne de formation, créée l'identité musicale des jeux."></TeamBox>
-                    <TeamBox img={eric} name="ORESKOV Eric" text='Directeur scénaristique "Je mets en place le scénario des jeux du studio".'></TeamBox>
+                    <TeamBox img={eric} name="ORESKOVIC Eric" text='Directeur scénaristique "Je mets en place le scénario des jeux du studio".'></TeamBox>
                     <TeamBox img={l} name="BASSAGAL Louis" text="Passionné de développement de mécaniques de jeu immersives."></TeamBox>
                 </div>
             </div>
--- src/components/Values.js
+++ src/components/Values.js
@@ -13,33 +13,18 @@ function Values() {
         <div className="value-text">
           <div className="value-text-container">
             <div>
-              <h1 className="value-title">
-                Turtles on Fire : L'excellence, le respect et la passion au cœur de notre création de jeux
-              </h1>
+              <h1 className="value-title">Turtles on Fire : Une petite équipe avec de grandes ambitions</h1>
               <p className="value-paragraph">
-                Dans le studio de développement de jeux vidéo Turtles on Fire, nous embrassons avec ferveur les valeurs
-                de la perfection, du respect des développeurs et de la sortie de jeux de qualité. Notre équipe est
-                animée par une passion inextinguible pour l'art du jeu vidéo et par la volonté constante de repousser
-                les frontières de l'excellence.
-                <li className='value-list'>
-                  <ul>
-                    Nous croyons fermement que chaque jeu que nous créons doit refléter notre quête incessante de la
-                    perfection. Chaque pixel, chaque ligne de code et chaque aspect du gameplay est méticuleusement
-                    conçu et poli pour offrir une expérience immersive et captivante aux joueurs du monde entier.
-                  </ul>
-                  <ul>
-                    Au sein de notre studio, le respect des développeurs est une priorité absolue. Nous considérons
-                    notre équipe comme notre plus grand atout et nous nous engageons à créer un environnement de travail
-                    où chacun se sent valorisé, écouté et soutenu. Nous encourageons la collaboration, la créativité et
-                    l'épanouissement personnel, car nous croyons que c'est ainsi que naissent les jeux exceptionnels.
-                  </ul>
-                  <ul>
-                    Enfin, notre engagement envers la sortie de bons jeux est ancré dans notre ADN. Nous mettons tout en
-                    œuvre pour offrir des expériences ludiques de qualité supérieure, en veillant à ce que chaque jeu
-                    réponde aux attentes élevées de notre communauté de joueurs. Nous nous efforçons de capturer
-                    l'imagination, de raconter des histoires mémorables et de procurer des moments de divertissement
-                    inoubliables.
-                  </ul>
+                Chez Turtles on Fire, on a commencé à six à faire des jeux ensemble, un peu pour le fun, mais surtout
+                guidé par la curiosité de voir à quoi ressemble les coulisses de création de notre média préféré. Et,
+                sans surprise, on a été conquis par cette expérience. C’est pour cela qu’aujourd’hui, on a décidé de
+                voir plus grand et de se lancer dans la production de jeux vidéo de manière professionnelle ! Bien que
+                l'on n'ait pas l’expérience, les compétences et les ressources pour y arriver facilement, c’est tout
+                l’enjeu de cette aventure :
+                <li className="value-list">
+                  <ul>Progresser dans la création de jeux</ul>
+                  <ul>Se développer par nos propres moyens</ul>
+                  <ul>Inclure notre communauté dans notre développement</ul>
                 </li>
               </p>
             </div>
